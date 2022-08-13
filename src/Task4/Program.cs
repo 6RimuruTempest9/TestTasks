d3e58a6e1ce1c44ce3fdb3a9e8a5f3e9e51e5932
Vitali Talatai <Vitali_Talatai@epam.com>
2022-08-14 00:33:27 +0300

    VT_master: completed Task 4.

---
+++ src/Task4/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var @base = InputBase();
+            var degree = InputDegree();
+
+            var number = Pow(@base, degree);
+
+            Console.WriteLine("Result: " + number);
+        }
+
+        private static float Pow(float @base, int degree)
+        {
+            return (degree == 0) ? 1
+                : (degree < 0) ? (1 / @base) * Pow(@base, degree + 1)
+                : @base * Pow(@base, degree - 1);
+        }
+
+        private static float InputBase()
+        {
+            var input = string.Empty;
+            var @base = default(float);
+
+            do
+            {
+                Console.Clear();
+
+                Console.Write("Base: ");
+
+                input = Console.ReadLine();
+            }
+            while (!float.TryParse(input, out @base));
+
+            return @base;
+        }
+
+        private static int InputDegree()
+        {
+            var input = string.Empty;
+            var degree = default(int);
+
+            do
+            {
+                Console.Clear();
+
+                Console.Write("Degree: ");
+
+                input = Console.ReadLine();
+            }
+            while (!int.TryParse(input, out degree));
+
+            return degree;
+        }
+    }
+}
