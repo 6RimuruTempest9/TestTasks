e9c98a3db44ffe625fd27e19698ff1952bd9fd08
Vitali Talatai <Vitali_Talatai@epam.com>
2022-08-14 00:05:56 +0300

    VT_master: completed Task 3.

---
+++ src/Task3/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var n = InputOrdinalNumber();
+
+            var number = FibonacciNumber(n);
+
+            Console.WriteLine(number);
+        }
+
+        private static long FibonacciNumber(int n)
+        {
+            if (n < 1)
+            {
+                throw new ArgumentException("Ordinal number must be more then 0!", nameof(n));
+            }
+
+            if (n == 1)
+            {
+                return 0;
+            }
+
+            return (n == 2) ? 1 : FibonacciNumber(n - 1) + FibonacciNumber(n - 2);
+        }
+
+        private static int InputOrdinalNumber()
+        {
+            var input = string.Empty;
+            var number = default(int);
+
+            do
+            {
+                Console.Clear();
+
+                Console.Write("Ordinal number: ");
+
+                input = Console.ReadLine();
+            }
+            while (!int.TryParse(input, out number) || number < 1);
+
+            return number;
+        }
+    }
+}
