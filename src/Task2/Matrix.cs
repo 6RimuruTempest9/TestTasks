fa14df50bebd02acc9ae9377a982af10cbfd60d8
Vitali Talatai <Vitali_Talatai@epam.com>
2022-08-13 22:52:37 +0300

    VT_master: completed Task 2.

---
+++ src/Task2/Matrix.cs
@@ -0,0 +1,90 @@
+﻿namespace Task2
+{
+    internal class Matrix
+    {
+        #region Fields
+
+        private float[][] _numbers;
+
+        #endregion
+
+        #region Constructors
+
+        private Matrix(float[][] numbers)
+        {
+            _numbers = numbers;
+        }
+
+        #endregion
+
+        #region Instance Methods
+
+        public float Sum(Func<float[], float> selector)
+        {
+            return _numbers.Sum(selector);
+        }
+
+        public override string ToString()
+        {
+            return string.Concat(_numbers.Select(row =>
+            {
+                var rowString = string.Join(' ', row.Select(number => string.Format("{0,-5}", number)));
+
+                return rowString + Environment.NewLine;
+            }));
+        }
+
+        #endregion
+
+        #region Factory Methods
+
+        public static Matrix FromTwoDimensionArray(float[][] numbers)
+        {
+            if (numbers == null)
+            {
+                throw new ArgumentNullException(nameof(numbers));
+            }
+
+            return new Matrix(numbers);
+        }
+
+        public static Matrix FromOneDimensionArray(int rowCount, int columnCount, float[] numbers)
+        {
+            if (rowCount < 1)
+            {
+                throw new ArgumentException("Row count can not be less than 1!", nameof(rowCount));
+            }
+
+            if (columnCount < 1)
+            {
+                throw new ArgumentException("Column count can not be less than 1!", nameof(columnCount));
+            }
+
+            if (numbers == null)
+            {
+                throw new ArgumentNullException(nameof(numbers));
+            }
+
+            if (numbers.Length != rowCount * columnCount)
+            {
+                throw new ArgumentException("The count of numbers must be equal to the multiplication of the row and column!", nameof(numbers));
+            }
+
+            var twoDimensionArray = new float[rowCount][];
+
+            for (var row = 0; row < rowCount; ++row)
+            {
+                twoDimensionArray[row] = new float[columnCount];
+
+                for (var column = 0; column < columnCount; ++column)
+                {
+                    twoDimensionArray[row][column] = numbers[row * rowCount + column];
+                }
+            }
+
+            return new Matrix(twoDimensionArray);
+        }
+
+        #endregion
+    }
+}
---
+++ src/Task2/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var rowCount = InputRowCount();
+            var columnCount = InputColumnCount();
+            var twoDimensionArray = InputTwoDimensionArray(rowCount, columnCount);
+
+            var matrix = Matrix.FromTwoDimensionArray(twoDimensionArray);
+
+            Console.WriteLine(matrix);
+
+            var sum = matrix.Sum(row => row.Where(number => number % 3 == 0).Sum());
+
+            Console.WriteLine("Sum of numbers of multiples of three: " + sum);
+        }
+
+        private static float InputNumber(string previewText)
+        {
+            var input = string.Empty;
+            var number = default(float);
+
+            do
+            {
+                Console.Clear();
+
+                Console.Write(previewText);
+
+                input = Console.ReadLine();
+            }
+            while (!float.TryParse(input, out number));
+
+            return number;
+        }
+
+        private static int InputRowCount()
+        {
+            var input = string.Empty;
+            var rowCount = default(int);
+
+            do
+            {
+                Console.Clear();
+
+                Console.Write("Row count: ");
+
+                input = Console.ReadLine();
+            }
+            while (!int.TryParse(input, out rowCount) || rowCount < 1);
+
+            return rowCount;
+        }
+
+        private static int InputColumnCount()
+        {
+            var input = string.Empty;
+            var columnCount = default(int);
+
+            do
+            {
+                Console.Clear();
+
+                Console.Write("Column count: ");
+
+                input = Console.ReadLine();
+            }
+            while (!int.TryParse(input, out columnCount) || columnCount < 1);
+
+            return columnCount;
+        }
+
+        private static float[][] InputTwoDimensionArray(int rowCount, int columnCount)
+        {
+            var twoDimensionArray = new float[rowCount][];
+
+            Enumerable.Range(0, rowCount).ToList().ForEach(rowIndex =>
+            {
+                twoDimensionArray[rowIndex] = new float[columnCount];
+
+                Enumerable.Range(0, columnCount).ToList().ForEach(columnIndex =>
+                {
+                    var previewText = string.Format("[{0}][{1}]: ", rowIndex + 1, columnIndex + 1);
+
+                    twoDimensionArray[rowIndex][columnIndex] = InputNumber(previewText);
+                });
+            });
+
+            return twoDimensionArray;
+        }
+    }
+}
